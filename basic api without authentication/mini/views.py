99d76a697a323c14c1f6ee0a368adb40ebd0a4d5
jayson7 <lexxiijoo70@gmail.com>
2023-11-17 16:28:32 +0100

    editing

--- basic api without authentication/mini/views.py
+++ basic api without authentication/mini/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# views.py
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Products
+from .serializers import ProductSerializer
+
+class ProductListView(APIView):
+    def get(self, request, *args, **kwargs):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
