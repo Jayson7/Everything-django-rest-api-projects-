6aa833a567325e56d9d0aff2436565b049110440
jayson7 <lexxiijoo70@gmail.com>
2023-11-19 14:06:52 +0100

    done

--- basic app without authentication (api view)/productapp/urls.py
+++ basic app without authentication (api view)/productapp/urls.py
@@ -1,9 +1,11 @@
 # urls.py
-
+from django.contrib import admin
 from django.urls import path
-from .views import product_list, product_detail
+from mini.views import product_list, product_detail
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('products/', product_list, name='product-list'),
     path('products/<int:pk>/', product_detail, name='product-detail'),
 ]
+
