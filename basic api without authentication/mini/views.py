8bff913587e23cd8f0b53cfc3faa6cd7c90d721e
jayson7 <lexxiijoo70@gmail.com>
2023-11-19 08:18:10 +0100

    editing

--- basic api without authentication/mini/views.py
+++ basic api without authentication/mini/views.py
@@ -1,5 +1,5 @@
 # views.py
-
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Products
@@ -7,6 +7,14 @@ from .serializers import ProductSerializer
 
 class ProductListView(APIView):
     def get(self, request, *args, **kwargs):
-        products = Product.objects.all()
+        products = Products.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):
+        
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- basic api without authentication/productApp/urls.py
+++ basic api without authentication/productApp/urls.py
@@ -2,9 +2,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from mini.views import ProductListView
+from mini.views import ProductListView, ProductDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', ProductListView.as_view(), name='product-list'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
 ]
